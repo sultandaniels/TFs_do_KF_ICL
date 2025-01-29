6845eb373057b3fd16ef65c37790057cf2dd7a75
sultandaniels <sultan_daniels@berkeley.edu>
2025-01-29 08:58:07 +0000

    gauss needle train conv

--- rename_file.py
+++ rename_file.py
@@ -4,10 +4,10 @@ from src.data_processing import gen_ckpt_steps
 def rename_files(directory, prefix):
     for filename in os.listdir(directory):
         if filename.startswith(prefix):
-            #find the index in the filename where the prefix ends
+            # find the index in the filename where the prefix ends
             # print(f"prefix: {prefix}\n filename: {filename}\n new filename: {filename[:len(prefix)] + filename[len(prefix)+11:]}\n\n\n")
-            # print(f"prefix: {prefix}\n filename: {filename}\n new filename: {"train_conv_" + filename[:7] + "haystack_len_4_" + filename[7:]}\n\n\n")
-            new_filename = "train_conv_" + filename[:7] + "haystack_len_4_" + filename[7:]
+            # print(f"prefix: {prefix}\n filename: {filename}\n new filename: {filename[:18] + "haystack_len_4_" + filename[18:42] + filename[53:]}\n\n\n")
+            new_filename = filename[:18] + "haystack_len_4_" + filename[18:42] + filename[53:]
             old_filepath = os.path.join(directory, filename)
             new_filepath = os.path.join(directory, new_filename)
             os.rename(old_filepath, new_filepath)
@@ -15,10 +15,10 @@ def rename_files(directory, prefix):
 
 if __name__ == "__main__":
 
-    ckpts = gen_ckpt_steps(3000, 105000, 3000)
+    ckpts = gen_ckpt_steps(108000, 180000, 3000)
     for ckpt in ckpts:
-        directory = f"/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250112_043028.07172b_multi_sys_trace_ortho_state_dim_5_ident_C_lr_1.584893192461114e-05_num_train_sys_40000/prediction_errors_ident_C_step={ckpt}.ckpt"  # Replace with the path to your directory
-        prefix = "needle_val_ortho_state_dim_5_"  # Replace with the old prefix
+        directory = f"/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250114_202420.3c1184_multi_sys_trace_gaussA_state_dim_10_gauss_C_lr_1.584893192461114e-05_num_train_sys_40000/prediction_errors_gauss_C_step={ckpt}.ckpt"  # Replace with the path to your directory
+        prefix = "train_conv_needle_val_gaussA_state_dim_10_"  # Replace with the old prefix
 
         if os.path.exists(directory):
             rename_files(directory, prefix)
