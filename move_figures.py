d9f813634b9a5f39c733f40179392611698f91be
sultandaniels <sultan_daniels@berkeley.edu>
2025-02-27 17:41:33 -0800

    moving files automation

--- move_figures.py
+++ move_figures.py
@@ -31,17 +31,26 @@ def move_files_and_directories(directories, model_names, destination_base):
                 print(f"Error moving directory {train_src} to {train_dest}: {e}")
 
 # Example usage
-directories = [
-    '/path/to/dir1',
-    '/path/to/dir2',
-    '/path/to/dir3'
-]
+directories = ['/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250112_043028.07172b_multi_sys_trace_ortho_state_dim_5_ident_C_lr_1.584893192461114e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250114_202420.3c1184_multi_sys_trace_gaussA_state_dim_10_gauss_C_lr_1.584893192461114e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250124_052617.8dd0f8_multi_sys_trace_ident_state_dim_5_ident_C_lr_1.584893192461114e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250125_103302.919337_multi_sys_trace_gaussA_state_dim_10_gauss_C_lr_3.169786384922228e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250125_104123.f75c04_multi_sys_trace_ortho_state_dim_5_ident_C_lr_3.169786384922228e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250125_110549.80eba5_multi_sys_trace_ident_state_dim_5_ident_C_lr_3.169786384922228e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250125_202437.caf35b_multi_sys_trace_gaussA_state_dim_10_gauss_C_lr_1.3207437987531975e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250125_204545.a2cee4_multi_sys_trace_ortho_state_dim_5_ident_C_lr_1.3207437987531975e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250125_210849.09203d_multi_sys_trace_ident_state_dim_5_ident_C_lr_1.3207437987531975e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250128_022150.04b6bf_multi_sys_trace_gaussA_state_dim_10_gauss_C_lr_6.339572769844456e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250128_022310.fc649a_multi_sys_trace_ident_state_dim_5_ident_C_lr_6.339572769844456e-05_num_train_sys_40000', '/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250128_022331.067361_multi_sys_trace_ortho_state_dim_5_ident_C_lr_6.339572769844456e-05_num_train_sys_40000']
+
+print(len(directories))
 
 model_names = [
-    'model1',
-    'model2',
-    'model3'
+    'ortho_med',
+    'gauss_med',
+    'ident_med',
+    'gauss_small',
+    'ortho_small',
+    'ident_small',
+    'gauss_big',
+    'ortho_big',
+    'ident_big',
+    'gauss_tiny',
+    'ident_tiny',
+    'ortho_tiny',
 ]
+
+
 
 destination_base = '/Users/sultandaniels/Documents/Transformer_Kalman/67b648fddceb6a3b3812dd6c/Arxiv_Interleaved Time-series show the Emergence of Associative Recall is no Mirage/needle/train_conv/seg_len_10'
 
