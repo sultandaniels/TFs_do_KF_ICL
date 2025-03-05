6f202aa1e2e0e6af06ad35372a23035f60820ea2
sultandaniels <sultan_daniels@berkeley.edu>
2025-03-05 21:25:19 +0000

    cleaning up BLISS server

--- upload_ckpts_huggingface.py
+++ upload_ckpts_huggingface.py
@@ -12,7 +12,7 @@ HfFolder.save_token(hf_token)
 api = HfApi()
 
 api.upload_large_folder(
-    repo_id="sultan-daniels/TFs_do_KF_ICL_ortho_med_GPT2_experiment",
+    repo_id="sultan-daniels/TFs_do_KF_ICL_ident_med_GPT2_experiment",
     repo_type="model",
-    folder_path="/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250112_043028.07172b_multi_sys_trace_ortho_state_dim_5_ident_C_lr_1.584893192461114e-05_num_train_sys_40000"
+    folder_path="/home/sultand/TFs_do_KF_ICL/outputs/GPT2/250124_052617.8dd0f8_multi_sys_trace_ident_state_dim_5_ident_C_lr_1.584893192461114e-05_num_train_sys_40000"
 )
