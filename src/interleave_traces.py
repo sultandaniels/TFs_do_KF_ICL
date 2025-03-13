8a2d16975de4432bcb24b601e40cd14f4f499969
sultandaniels <sultan_daniels@berkeley.edu>
2025-03-13 01:15:20 +0000

    interleave traces script

--- src/interleave_traces.py
+++ src/interleave_traces.py
@@ -1,3 +1,19 @@
+##############################
+#multiline comment
+""""
+multi_sys_ys: np array, shape (num_test_traces_configs, num_traces_per_config, trace length, ny + special token dim), this holds the prompts
+sys_choices_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the order of the systems in the haystack. The system indices correspond to the unintereaved system corpus
+sys_dict_per_config: list of dictionaries, length num_test_traces_configs, each dictionary's keys are the system index in the entire uninterleaved system corpus and the values are the system index for the subset of systems chosen for each trace
+tok_seg_lens_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the lengths of the segments in the interleaved traces including special tokens
+seg_starts_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the starting indices of the segments in the interleaved traces
+real_seg_lens_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the lengths of the segments in the interleaved traces excluding special tokens
+sys_inds_per_config: list of lists, shape (num_test_traces_configs, # of systems chosen for trace), this holds the subset of systems chosen for each trace
+"""
+
+#####################
+
+
+
 from create_plots_with_zero_pred import interleave_traces
 #import the config
 from core import Config
@@ -62,18 +78,5 @@ if __name__ == "__main__":
     with open(filename, 'wb') as f:
         pickle.dump(file_dict, f)
     print(f"Saved interleaved traces to {filename}")
-
-
-    ##############################
-    #multiline comment
-    """"
-    multi_sys_ys: np array, shape (num_test_traces_configs, num_traces_per_config, trace length, ny + special token dim), this holds the prompts
-    sys_choices_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the order of the systems in the haystack. The system indices correspond to the unintereaved system corpus
-    sys_dict_per_config: list of dictionaries, length num_test_traces_configs, each dictionary's keys are the system index in the entire uninterleaved system corpus and the values are the system index for the subset of systems chosen for each trace
-    tok_seg_lens_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the lengths of the segments in the interleaved traces including special tokens
-    seg_starts_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the starting indices of the segments in the interleaved traces
-    real_seg_lens_per_config: list of lists, shape (num_test_traces_configs, # of segments in the prompt), this holds the lengths of the segments in the interleaved traces excluding special tokens
-    sys_inds_per_config: list of lists, shape (num_test_traces_configs, # of systems chosen for trace), this holds the subset of systems chosen for each trace
-    """
     
 
