0d90d72cae7f591611227400cff1abcb2d520324
sultandaniels <sultan_daniels@berkeley.edu>
2025-05-09 18:48:24 -0700

    before system batching

--- src/haystack_plots.py
+++ src/haystack_plots.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Liberation Serif']
 
 import logging
 import pickle
